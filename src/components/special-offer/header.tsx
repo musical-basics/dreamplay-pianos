5213eacdb67c517d5b8e87d8b7a8e305ac3a00f8
Lionel Yu <support@musicalbasics.com>
2026-03-12 19:23:33 -0700

    Logo click navigates to /intro-offer

--- src/components/special-offer/header.tsx
+++ src/components/special-offer/header.tsx
@@ -114,10 +114,10 @@ export function SpecialOfferHeader({ forceOpaque = false, darkMode = false, clas
                     <div className="container mx-auto px-4 h-16 flex items-center justify-between">
                         {/* Logo */}
                         <a
-                            href="/"
+                            href="/intro-offer"
                             onClick={(e) => {
                                 e.preventDefault()
-                                router.push(getHomepage())
+                                router.push("/intro-offer")
                             }}
                             className="flex items-center gap-2 cursor-pointer"
                         >
