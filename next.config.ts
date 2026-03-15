e5659d0331e811bf4479a6bd71a82e9297e2979d
Lionel Yu <support@musicalbasics.com>
2026-03-15 07:28:35 -0700

    feat: rewrite /buy-product to /checkout-pages/buy-product

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,14 @@ const nextConfig: NextConfig = {
       },
     ];
   },
+  async rewrites() {
+    return [
+      {
+        source: '/buy-product',
+        destination: '/checkout-pages/buy-product',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
