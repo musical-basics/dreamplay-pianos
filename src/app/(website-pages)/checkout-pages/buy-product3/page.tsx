f2a8616773685470a7502be262f7931b20f1cfe6
Lionel Yu <support@musicalbasics.com>
2026-03-15 07:50:39 -0700

    fix: update image paths on buy-product3 to match current buy-product

--- src/app/(website-pages)/checkout-pages/buy-product3/page.tsx
+++ src/app/(website-pages)/checkout-pages/buy-product3/page.tsx
@@ -26,7 +26,7 @@ export default function BuyProduct3Page() {
 
                             <div className="relative w-full max-w-4xl mx-auto aspect-[16/9] md:aspect-[2/1] animate-fade-in-up delay-200">
                                 <Image
-                                    src="/images/DS6.0-Black-1.png"
+                                    src="/images/keyboards/ds60-black-narrow-keys-piano.png"
                                     alt="DreamPlay One"
                                     fill
                                     className="object-contain transform scale-125"
@@ -49,7 +49,7 @@ export default function BuyProduct3Page() {
                                 {/* Benefit 1 */}
                                 <div className="flex items-start gap-4">
                                     <div className="w-8 h-8 flex-shrink-0 relative mt-1">
-                                        <Image src="/images/Frame-85.svg" alt="Price Icon" fill className="object-contain" />
+                                        <Image src="/images/icons/Frame-85.svg" alt="Price Icon" fill className="object-contain" />
                                     </div>
                                     <div>
                                         <p className="font-medium text-lg text-neutral-900 leading-snug">
@@ -61,7 +61,7 @@ export default function BuyProduct3Page() {
                                 {/* Benefit 2 */}
                                 <div className="flex items-start gap-4">
                                     <div className="w-8 h-8 flex-shrink-0 relative mt-1">
-                                        <Image src="/images/Vector-6.svg" alt="Journal Icon" fill className="object-contain" />
+                                        <Image src="/images/icons/Vector-6.svg" alt="Journal Icon" fill className="object-contain" />
                                     </div>
                                     <div>
                                         <p className="font-medium text-lg text-neutral-900 leading-snug">
@@ -73,7 +73,7 @@ export default function BuyProduct3Page() {
                                 {/* Benefit 3 */}
                                 <div className="flex items-start gap-4">
                                     <div className="w-8 h-8 flex-shrink-0 relative mt-1">
-                                        <Image src="/images/Frame-84.svg" alt="Music Icon" fill className="object-contain" />
+                                        <Image src="/images/icons/Frame-84.svg" alt="Music Icon" fill className="object-contain" />
                                     </div>
                                     <div>
                                         <p className="font-medium text-lg text-neutral-900 leading-snug">
@@ -85,7 +85,7 @@ export default function BuyProduct3Page() {
                                 {/* Benefit 4 */}
                                 <div className="flex items-start gap-4">
                                     <div className="w-8 h-8 flex-shrink-0 relative mt-1">
-                                        <Image src="/images/Frame-82.svg" alt="Shipping Icon" fill className="object-contain" />
+                                        <Image src="/images/icons/Frame-82.svg" alt="Shipping Icon" fill className="object-contain" />
                                     </div>
                                     <div>
                                         <p className="font-medium text-lg text-neutral-900 leading-snug">
